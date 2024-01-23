280281803db40cc311ec353c9ba245318bcc0ad0
manishteknikoglobal <manish.kumar@teknikoglobal.in>
2024-01-23 11:07:02 +0530

    user auth some changs

--- controllers/user/auth/verifySignupOTP.js
+++ controllers/user/auth/verifySignupOTP.js
@@ -10,12 +10,14 @@ const verifySignupOTP = async (req, res, next) => {
         if (!phone_number) throw new ApiError("Phone Number is required!", 400);
         if (isNaN(phone_number)) throw new ApiError("Phone Number is invalid!", 400);
         if (!otp) throw new ApiError("OTP is required!", 400);
-
-        const user = await User.findOne({ phone_number, signup_otp: otp }).select("role").lean();
-        if (!user) throw new ApiError("Incorrect OTP!", 400);
+        // 
+        // const user = await User.findOne({ phone_number, signup_otp: otp }).select("role").lean();
+        const user = await User.findOne({ phone_number });
+        if (!user) throw new ApiError("no user find with this phone number!", 400);
+        if(user.signup_otp !== String(otp)) throw new ApiError("Incorrect OTP!", 400);
 
         const token = signJWT(user._id);
-        res.status(201).json({ status: true, message: "User signed up successfully.", data: { token } });
+        res.status(201).json({ status: true, message: "User signed in successful", data: { token } });
     } catch (error) {
         next(error);
     }
